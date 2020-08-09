b248963d80cd770dfc04891f4673c8988eae9926
kritvikalani <67899380+kritvikalani@users.noreply.github.com>
2020-08-09 12:13:44 +0530

    Update polygon.js

--- polygon.js
+++ polygon.js
@@ -17,7 +17,7 @@ class Polygon{
     display() {
         var pos= this.body.position
         ellipseMode(RADIUS)
-        fill(100)
+        fill(220)
         ellipse(/*this.image,*/pos.x,pos.y,this.body.radius,this.body.radius)
     }
 }
