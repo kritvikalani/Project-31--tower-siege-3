49506ecf577ecc71412194d9c4016007b6bbfbab
kritvikalani <67899380+kritvikalani@users.noreply.github.com>
2020-08-09 12:15:37 +0530

    Update sling.js

--- sling.js
+++ sling.js
@@ -24,7 +24,7 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            stroke(210);
+            stroke(0);
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
